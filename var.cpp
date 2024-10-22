2fea43cf17e49737db440321f760f1f181169ea1
zjzjzjzj1874 <zjzjzjzj1874@gmail.com>
2024-10-22 09:35:30 +0800

    feat: var in cpp
    Signed-off-by: zjzjzjzj1874 <zjzjzjzj1874@gmail.com>

--- var.cpp
+++ var.cpp
@@ -7,7 +7,12 @@
 
 using namespace std;
 
+// 申明全局变量 => 可以不初始化，会被编译器默认初始化为对应类型的零值
+int number; // 等价于 int number = 0;
+
 void var() {
+    cout << "----------变量定义篇章-Start-----------\n";
+
     // 定义变量，局部变量，最好定义的时候就赋值，否则使用的时候没有赋值，会报错
     int a = 0, b= 0;
     cout << " before a == " << a << endl;
@@ -18,4 +23,15 @@ void var() {
     b = 100;
     cout << " after b == " << b << endl;
     cout << " after a == " << a << endl;
+
+    int number = 1;
+    cout << "局部：number = " << number << endl;
+    cout << "全局：number = " << ::number << endl;
+
+
+    const float Pi = 3.14159;
+    cout << "PI: " << PI << endl;
+    cout << "Pi: " << Pi << endl;
+
+    cout << "----------变量定义篇章-End-----------\n";
 };
--- var.h
+++ var.h
@@ -5,6 +5,10 @@
 #ifndef VAR_H
 #define VAR_H
 
+// 这里是宏定义
+#define ZERO 0
+#define PI 3.14159
+
 // 变量的函数
 void var();
 
